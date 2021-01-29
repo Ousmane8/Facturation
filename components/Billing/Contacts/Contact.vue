892415c2413ab4341b364a09ce12e0d10648c3e0
Ousmane Sylla <ousmane.sylla@lca-group.net>
2021-01-29 14:13:26 +0100

    contact vue

--- components/Billing/Contacts/Contact.vue
+++ components/Billing/Contacts/Contact.vue
@@ -1,7 +1,51 @@
 <template>
-  <div class="how-section1">
+  <table class="border-collapse w-full section">
+    <thead>
+    <tr>
+      <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Id</th>
+      <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Company name</th>
+      <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Siret</th>
+      <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Email</th>
+      <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Tel</th>
+      <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Adresse</th>
+      <th class="p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell">Actions</th>
+    </tr>
+    </thead>
+    <tbody>
 
-  </div>
+    <tr class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
+      <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+        <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Id</span>
+
+      </td>
+      <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+        <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Company name</span>
+
+      </td>
+      <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+        <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Siret</span>
+        <span class="rounded py-1 px-3 text-xs font-bold"></span>
+      </td>
+      <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+        <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Email</span>
+        <span class="rounded py-1 px-3 text-xs font-bold"></span>
+      </td>
+      <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+        <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Tel</span>
+        <span class="rounded py-1 px-3 text-xs font-bold"></span>
+      </td>
+      <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+        <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Adresse</span>
+        <span class="rounded py-1 px-3 text-xs font-bold"></span>
+      </td>
+      <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+        <span class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Actions</span>
+        <a href="#" class="text-blue-400 hover:text-blue-600 underline"><i class="fas fa-edit"></i></a>
+        <a href="#" class="text-blue-400 hover:text-blue-600 underline pl-6"><i class="fas fa-trash"></i></a>
+      </td>
+    </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
@@ -11,7 +55,8 @@ export default {
 </script>
 
 <style scoped>
-.how-section1{
-  height: 75vh;
+.section{
+margin-top: 2%;
+margin-bottom: 50%;
 }
 </style>
